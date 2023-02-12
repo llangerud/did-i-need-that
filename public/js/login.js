d5939c249fbeb63b731c68189b90aa7350c69267
kristermyr <krister90@gmail.com>
2023-02-12 09:50:40 -0700

    login works

--- public/js/login.js
+++ public/js/login.js
@@ -5,9 +5,9 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
     const email = document.querySelector('#email-login').value.trim();
     if (email && password) {
-      
+      console.log(JSON.stringify({ email, password }))
       const response = await fetch('/api/users/login', {
-        method: 'post',
+        method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
