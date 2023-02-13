fc83e7b717020b8ac73b2fc95b83ce583bd8c8e3
lileee <112982735+lileeeeeee@users.noreply.github.com>
2023-02-13 10:20:37 -0600

    changed config back/problem was missing add-on

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,15 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL);
-// removed ?
-  // new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+      port:3306,
+    });
 
-
-  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  //     host: 'localhost',
-  //     dialect: 'mysql',
-  //     dialectOptions: {
-  //       decimalNumbers: true,
-  //     },
-    // });
-
-
-
-    module.exports = sequelize;
+module.exports = sequelize;
